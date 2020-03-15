97c360422b09928ec61cf01670611359e598ef8c
scarpino <scarpino@utexas.edu>
2020-03-15 19:57:54 -0400

    updating figure to include other approx

--- scripts/plot_R0s.R
+++ scripts/plot_R0s.R
@@ -46,9 +46,11 @@ col_val <- round((Z_mean/max(Z_mean, na.rm = TRUE) * 10)-1)
 col_val[which(col_val < 1)] <- 1
 fill <- cols[col_val]
 
-ord <-rev(c(4, 6, 7, 8, 2, 9, 1))
+ord <-rev(c(4, 6, 7, 10, 8, 2,1))
 quartz()
 par(mar = c(5, 10, 4, 2))
 bp <- barplot(Z_mean[ord], names = diseases[ord], col = fill[ord], las = 2, horiz = TRUE, cex.names = 0.8, xlim = c(0, 1), xlab = "Proportion of susceptible individuals infected", main = "")
 segments(x0 = Z_min[ord], y0 = bp[,1], x1 = Z_max[ord], y1 = bp[,1])
 points(spread_data$`Prop I`[ord], bp[,1], col = "#b2182b", pch = 16, cex = 1.5)
+points(1-(1/spread_data$R0[ord]), bp[,1], col = "#bababa", pch = 15, cex = 1.5)
+points(1-exp(-spread_data$R0[ord]), bp[,1], col = "#4d4d4d", pch = 17, cex = 1.5)
